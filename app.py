420bb718fc64573cbb83fc0db582cb1765ad3d92
Liliana Guadalupe Trejo <2798122@vikes.csuohio.edu>
2025-11-16 10:24:38 -0500

    fixed an error

--- app.py
+++ app.py
@@ -4,14 +4,16 @@ from flashcards import multiples_card
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 @app.route("/quiz")
 def quiz():
-    multiple = request.args.get("multiple", type=int, default=1)# Get selected multiple
-    deck = multiples_card(multiple)# Create flashcard deck in Python
-    return render_template("next_page.html", multiple=multiple, deck=deck) # Send deck + the chosen number to HTML
+    multiple = request.args.get("max_factor", type=int)#Get number user typed in index.html
+    if not multiple: #If user typed nothing or invalid, default to 1
+        multiple = 1
+    deck = multiples_card(multiple)#create deck using the correct number
+    return render_template("next_page.html", multiple=multiple, deck=deck)
 
 if __name__ == "__main__":
     app.run(debug=True)
